821e411ca0494652528680db88fc1ba465fa5010
ming <mingruqi@sina.cn>
2020-01-05 23:29:12 +0800

    构建程序结构

--- google/js/bangke_url_m.js
+++ google/js/bangke_url_m.js
@@ -1,2 +1,2 @@
-var baseServerUrl= 'https://www.qinchacha.com/';//服务器域名或ip
+var baseServerUrl= 'https://demo.22com.cn/BangKeServer/';//服务器域名或ip
 var url_initData='https://www.huopengpeng.com/plug/index/popup/';
